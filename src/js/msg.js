249a1cffa94b407754fd58080891425eae2ac3ba
Marcos Castro <marcoscv@gmail.com>
2019-03-30 13:09:39 +0100

    update

--- src/js/msg.js
+++ src/js/msg.js
@@ -1,13 +1,11 @@
-function quitarVideoSin() {
+$('#videoSin').click(function() {
     $('#maquina').removeClass('hide');
     $('#videoRadar').removeClass('hide');
-    //$('#videoSin').addClass('hide');
     $('#videoSin').remove();
-}
+});
 
-$('#videoSin').click(function() {
-    quitarVideoSin();
-    maquina(texto, intervalo);
+ClueClient.receiveMessages(message => {
+    maquina(message, 30);
 });
 
 function maquina(texto, intervalo) {
@@ -53,14 +51,14 @@ function maquina(texto, intervalo) {
 
 // TEMP
 
-var texto = "Aquí va la pista TAMAÑO MEDIO que te damos porque eres un paquete, probamos otra línea más y más y más líneas";
-var intervalo = 30;
+// var texto = "Aquí va la pista TAMAÑO MEDIO que te damos porque eres un paquete, probamos otra línea más y más y más líneas";
+// var intervalo = 30;
 
-setTimeout(function(){
-    quitarVideoSin();
-    maquina(texto, intervalo);
-}, 10000);
+// setTimeout(function(){
+//     quitarVideoSin();
+//     maquina(texto, intervalo);
+// }, 10000);
 
-setTimeout(function(){
-    maquina('Aquí va la pista LAAARGA en texto pequeño y que te damos porque eres un paquete. Aquí va la pista que te damos porque eres un paquete. Aquí va la pista que te damos porque eres un paquete', 50);
-}, 20000);
+// setTimeout(function(){
+//     maquina('Aquí va la pista LAAARGA en texto pequeño y que te damos porque eres un paquete. Aquí va la pista que te damos porque eres un paquete. Aquí va la pista que te damos porque eres un paquete', 50);
+// }, 20000);
