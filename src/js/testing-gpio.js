ab4bd427c2a889038e3d5888916b033ec0fb0811
Marcos Castro <marcoscv@gmail.com>
2019-03-30 01:37:38 +0100

    update

--- src/js/testing-gpio.js
+++ src/js/testing-gpio.js
@@ -9,3 +9,31 @@ $(".container").delegate(".btn-remove", "click", function(){
         node.remove();
     });
 });
+
+$('.btn-generate').click(function() {
+    // objects
+    var allFormsVal = [];
+    var allLabels = [];
+
+    $(".container .form-control").each(function(){
+        allFormsVal.push($(this).val());
+    });
+
+    $(".container label").each(function(){
+        allLabels.push($(this).text());
+    });
+
+    // print object
+    jQuery.each((allFormsVal), function(i, val) {
+        $('.modal-body').append(i + " : " + val + "<br/>");
+        console.log(val);
+    });
+
+    jQuery.each((allLabels), function(i, val) {
+        $('.modal-body').append(i + " : " + val + "<br/>");
+        console.log(val);
+    });
+
+    // show modal
+    $('#modal').modal('show');
+});
